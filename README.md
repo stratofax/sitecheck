4cf2e5bbf000552d68b3a68ad9178c6b65bec2a7
Neil Johnson <neil@cadent.com>
2023-10-05 16:07:33 -0400

    add list_divs.py

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 Tools to check a website and create reports on the site's health.
 
+## Install and run this code
+
+We use poetry to manage our dependencies in this project.
+
+After you've cloned this repo onto your local machine, run:
+
+```bash
+cd /path/to/sitecheck-repo/
+poetry install
+cd /src/sitecheck
+poetry run python -m spider.py
+```
+
 ## spider.py
 
-Extracts a list of unique links from the specified page. Based on a ChatGPT-generated snippet.
+Extracts a list of unique links from the specified page.
+
+## list_divs.py
---
+++ src/sitecheck/list_divs.py
@@ -0,0 +1,29 @@
+"""
+List all the div tags in a page
+"""
+
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def list_divs(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup.find_all("div")
+
+
+if __name__ == "__main__":
+    # Check for a command line argument
+    url = sys.argv[1] if len(sys.argv) > 1 else "https://example.com/"
+    divs = list_divs(url)
+
+    if divs is not None:
+        print(f"Found {len(divs)} divs on the page {url}:")
+        for div in divs:
+            print(div)
+    else:
+        print(f"Failed to fetch the HTML content from {url}")
