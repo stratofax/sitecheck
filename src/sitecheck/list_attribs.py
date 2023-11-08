9da7f532e0b4966d604999918aba036698e122a2
Neil Johnson <neil@cadent.com>
2023-11-07 23:34:44 -0500

    list_attribs with argparse and tests

--- src/sitecheck/list_attribs.py
+++ src/sitecheck/list_attribs.py
@@ -5,17 +5,39 @@ from bs4 import BeautifulSoup
 
 
 def find_elements(html: str, element: str) -> list:
+    """
+    Find elements in HTML.
+
+    Args:
+        html (str): The HTML content.
+        element (str): The HTML element to find.
+
+    Returns:
+        list: A list of elements matching the given element.
+
+    """
     soup = BeautifulSoup(html, "html.parser")
     return soup.find_all(element)
 
 
 def find_attribs(elements: list, attrib: str) -> list:
+    """
+    Find attributes of elements.
+
+    Args:
+        elements (list): A list of HTML elements.
+        attrib (str): The attribute of the element to find.
+
+    Returns:
+        list: A list of attribute values matching the given attribute.
+
+    """
     return [
         element.get(attrib) for element in elements if element.get(attrib) is not None
     ]
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser(
         description="Find elements and their attributes in HTML."
     )
