cf7119e18a96d6a67a8c9328b13e5a1e909c9f27
Neil Johnson <neil@cadent.com>
2023-11-14 17:07:04 -0500

    Updated tags

--- src/sitecheck/get_cadent_meta.py
+++ src/sitecheck/get_cadent_meta.py
@@ -19,6 +19,16 @@ def parse_meta(html: str):
     post_title = clean_title(soup.title.string)
     post_date = soup.time["datetime"]
     tags = [tag.text for tag in soup.find_all("a", attrs={"rel": "tag"})]
+    # write post_title to post_title.txt
+    # write post_date to post_date.txt
+    # write tags to tags.txt
+    with open("post_title.txt", "w") as f:
+        f.write(post_title)
+    with open("post_date.txt", "w") as f:
+        f.write(post_date)
+    with open("tags.txt", "w") as f:
+        for tag in tags:
+            f.write(f"{tag},")
 
     return {"post_title": post_title, "post_date": post_date, "tags": tags}
 
